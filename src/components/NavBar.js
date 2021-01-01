5b8c95ad30fbd8d9d5a5347fe8d94973424051bb
matthewxvoorhees <matthew.crnich@gmail.com>
2021-01-01 15:31:32 -0700

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,36 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <NavLink
+          to="/"
+          exact
+          activeStyle={{
+            background: 'pink'
+          }}
+        >Home   -</NavLink>
+        <NavLink
+          to="/movies"
+          exact
+          activeStyle={{
+            background: 'pink'
+          }}
+        > Movies   -</NavLink>
+        <NavLink
+          to="/directors"
+          exact
+
+          activeStyle={{
+            background: 'pink'
+          }}
+        >Directors   -</NavLink>
+        <NavLink
+          to="/actors"
+          exact
+
+          activeStyle={{
+            background: 'pink'
+          }}
+        > Actors   </NavLink>
     </div>
   );
 };
